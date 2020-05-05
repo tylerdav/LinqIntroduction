95d2c9ae2a4188537d5dc0c8f7a0251610069ba3
tylerdav <tdavis1433@att.net>
2020-05-05 15:18:09 -0500

    using extension method, lambda expressions, func/action types, and using var

---
+++ Features/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Features
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Features/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Features.Linq;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) =>
+            {
+                int temp = x + y;
+                return temp;
+            };
+
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(3, 5)));
+
+            var develpers = new Employee[]
+            {
+                new Employee { Id = 1, Name= "Tyler"},
+                new Employee { Id = 2, Name= "Scott"}
+            };
+
+            var sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            var query = develpers.Where(e => e.Name.Length == 5)
+                                              .OrderBy(e => e.Name);
+
+            foreach (var employee in query)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
+        }
+    }
+}
