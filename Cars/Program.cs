11fa4cf38d8e5f520a3c212667f93eae312875d5
tylerdav <tdavis1433@att.net>
2020-05-06 14:26:27 -0500

    new project Cars

---
+++ Cars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = processFie("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> processFie(string path)
+        {
+            return
+            File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv)
+                .ToList();
+        }
+    }
+}
