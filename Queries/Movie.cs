375073cdcf08e15f500e8a934460bb71458e21ee
tylerdav <tdavis1433@att.net>
2020-05-06 09:34:29 -0500

    Introduction

---
+++ Queries/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        public int Year { get; set; }
+    }
+}
