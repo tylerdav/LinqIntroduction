f355c3d6a5cfea15bf55de463aa80de6dd780d2e
tylerdav <tdavis1433@att.net>
2020-05-08 16:07:40 -0500

    using joining, grouping, and Aggregating

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Cars
 {
@@ -9,51 +10,84 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = processFie("fuel.csv");
+            var cars = processCars("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
 
             var query =
                 from car in cars
-                where car.Manufacturer == "BMW" && car.Year == 2016
-                orderby car.Combined descending, car.Name ascending
+                group car by car.Manufacturer into carGroup
                 select new
                 {
-                    car.Manufacturer,
-                    car.Name,
-                    car.Combined
-                };
+                    Name = carGroup.Key,
+                    Max = carGroup.Max(c => c.Combined),
+                    Min = carGroup.Min(c => c.Combined),
+                    Avg = carGroup.Average(c => c.Combined)
+                } into result
+                orderby result.Max descending
+                select result;
 
-            var result = cars.SelectMany(c => c.Name)
-                             .OrderBy(c => c);
+            //extension method syntax
+            var query2 =
+                cars.GroupBy(c => c.Manufacturer)
+                    .Select(g =>
+                    {
+                        var results = g.Aggregate(new CarStatistics(),
+                                            (acc, c) => acc.Accumulate(c),
+                                            acc => acc.Compute());
+                        return new
+                        {
+                            Name = g.Key,
+                            Avg = results.Average,
+                            Min = results.Min,
+                            Max = results.Max
+                        };
+                    })
+                    .OrderByDescending(r => r.Max);
 
-                foreach(var character in result)
-                {
-                    Console.WriteLine(character);
-                }
-
-
-            //var top = cars.Where(c => c.Manufacturer =="BMW" && c.Year == 2016)
-            //                .OrderByDescending(c => c.Combined)
-            //                .ThenBy(c => c.Name)
-            //                .Select(c => c)
-            //                .First();
-
-            //var result = cars.Any(c => c.Manufacturer == "Ford");
-
-            Console.WriteLine(result);
-
-            foreach (var car in query.Take(10))
+            foreach (var result in query)
             {
-                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+                Console.WriteLine($"{result.Name}");
+                Console.WriteLine($"\t Max: {result.Max}");
+                Console.WriteLine($"\t Min: {result.Min}");
+                Console.WriteLine($"\t Avg: {result.Avg}");
             }
+
+
+            //var query =
+            //    from car in cars
+            //    join manufacturer in manufacturers
+            //        on new { car.Manufacturer, car.Year }
+            //        equals
+            //        new { Manufacturer = manufacturer.Name, manufacturer.Year }
+            //    orderby car.Combined descending, car.Name ascending
+            //    select new
+            //    {
+            //        manufacturer.Headquarters,
+            //        car.Name,
+            //        car.Combined
+            //    };
+            //var query2 =
+            //    cars.Join(manufacturers,
+            //                c => new { c.Manufacturer, c.Year },
+            //                m => new { Manufacturer = m.Name, m.Year }, 
+            //                (c, m) => new
+            //                {
+            //                    m.Headquarters,
+            //                    c.Name,
+            //                    c.Combined
+            //                })
+            //            .OrderByDescending(c => c.Combined)
+            //            .ThenBy(c => c.Name);
+
+
+            //foreach (var car in query.Take(10))
+            //{
+            //    Console.WriteLine($"{car.Headquarters} {car.Name} : {car.Combined}");
+            //}
         }
 
-        private static List<Car> processFie(string path)
+        private static List<Car> processCars(string path)
         {
-            //var query =
-            //    from line in File.ReadAllLines(path).Skip(1)
-            //    where line.Length > 1
-            //    select Car.ParseFromCsv(line);
-
             var query =
                 File.ReadAllLines(path)
                     .Skip(1)
@@ -62,13 +96,60 @@ namespace Cars
 
             return query.ToList();
         }
+
+        private static List<Manufacturer> ProcessManufacturers(string path)
+        {
+            var query =
+                File.ReadAllLines(path)
+                .Where(l => l.Length > 1)
+                .Select(l =>
+                {
+                    var columns = l.Split(',');
+                    return new Manufacturer
+                    {
+                        Name = columns[0],
+                        Headquarters = columns[1],
+                        Year = int.Parse(columns[2])
+                    };
+                });
+            return query.ToList();
+        }
+    }
+
+    public class CarStatistics
+    {
+        public CarStatistics()
+        {
+            Max = Int32.MinValue;
+            Min = Int32.MaxValue;
+        }
+        public CarStatistics Accumulate(Car car)
+        {
+            Count += 1;
+            Total += car.Combined;
+            Max = Math.Max(Max, car.Combined);
+            Min = Math.Min(Min, car.Combined);
+            return this;
+        }
+
+        public CarStatistics Compute()
+        {
+            Average = Total / Count;
+            return this;
+        }
+
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public int Total { get; set; }
+        public int Count { get; set; }
+        public double Average { get; set; }
     }
 
     public static class CarExtensions
     {
         public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
         {
-            foreach(var line in source)
+            foreach (var line in source)
             {
                 var columns = line.Split(',');
 
